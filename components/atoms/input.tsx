5c0597e86b2f3c657b408ab9385701403918e57f
tayv <48400779+tayv@users.noreply.github.com>
2022-04-16 23:00:47 -0600

    added types and classnames package to text input for dynamic styles such as size and input type

--- components/atoms/input.tsx
+++ components/atoms/input.tsx
@@ -1,13 +1,33 @@
-import React, { FC, forwardRef} from 'react'
+import React, { FC, forwardRef, InputHTMLAttributes, DetailedHTMLProps} from 'react'
+import classNames from 'classnames'
 
-export const Input = forwardRef(
+// TYPES
+  // Goal is to use this input component for all single line inputs (phone, numbers, text input)
+export type InputSize = "standard" | "large"
+export type InputType = "text" | "email" | "tel" | "number"
+export type InputProps = {
+  name: string
+  label: string
+  type?: InputType
+  size?: InputSize
+  className?: string
+} & Omit <DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>, "size">
+
+// DYNAMIC STYLING
+  // Using maps so full Tailwind classes can be seen for purging https://tailwindcss.com/docs/optimizing-for-production#writing-purgeable-html
+const inputSizeMap: {[key in InputSize]: string} = {
+  standard: "py-2 px-3",
+  large: "w-full py-3 px-4",
+}
+
+export const Input: FC<InputProps> = forwardRef<HTMLInputElement, InputProps>(
   (
     {
-      id,
       name,
       label,
-      type = 'text',
-      className = 'mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm',
+      type = "text",
+      size = "standard",
+      className = "", // to pass custom one-off styling
       ...props
     },
     ref
@@ -15,16 +35,36 @@ export const Input = forwardRef(
     return (
       <div className="max-w-sm">
         <label htmlFor="text" className="block text-sm font-medium text-gray-700">
-          { label }
+          {label}
         </label>
         <input
           ref={ref}
+          id={name}
           name={name}
           type={type}
           aria-label={label}
-          className={className}
+          className={
+            classNames([
+              "mt-1 block border border-gray-300 bg-white rounded-md shadow-sm", 
+              inputSizeMap[size], 
+              className,
+            ])
+          }
         />
       </div>
     )
   }
 )
+
+// HELPFUL SOURCES
+  
+// REUSABLE TYPESCRIPT COMPONENTS
+  // https://www.thisdot.co/blog/how-to-create-reusable-form-components-with-react-hook-forms-and-typescript
+  // https://codesandbox.io/s/react-hook-form-controlled-form-k3sz7?file=/src/index.tsx
+
+// FORWARDING REFS
+  // https://felixgerschau.com/react-forwardref-explained/ 
+  // https://reactjs.org/docs/forwarding-refs.html 
+
+// USING CLASSNAMES PACKAGE TO COMBINE & MAKE CONDITIONAL CLASSES
+  // https://nikitahl.com/how-to-assign-multiple-classes-in-react/ 
