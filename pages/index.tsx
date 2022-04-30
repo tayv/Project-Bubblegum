24db054c20c4c276f265b9bcb7852ae35c5c40ca
tayv <48400779+tayv@users.noreply.github.com>
2022-04-30 17:07:11 -0600

    updated landing page copy

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,37 +11,37 @@ export default function Home() {
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">Welcome 👋</h1>
+        <h1 className="text-6xl font-bold">Bubblegum</h1>
 
-        <p className="mt-3 text-2xl">This is a test project</p>
+        <p className="mt-3 text-2xl">Reusable react form components (wip 🏗️)</p>
 
         <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
           
           <Link href="/quiz1/pg1">
             <a className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600">
-              <h3 className="text-2xl font-bold">Quiz Template #1&rarr;</h3>
-              <p className="mt-4 text-xl">Try the first quiz!</p>
+              <h3 className="text-2xl font-bold">Multiple choice &rarr;</h3>
+              <p className="mt-4 text-xl">Radios and checkboxes live here.</p>
             </a>
           </Link>
             
           <Link href="/quiz1/pg2">
             <a className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600">
-              <h3 className="text-2xl font-bold">Quiz Template #2&rarr;</h3>
-              <p className="mt-4 text-xl">Try the second quiz!</p>
+              <h3 className="text-2xl font-bold">Lists &rarr;</h3>
+              <p className="mt-4 text-xl">You'll find dropdowns and comboboxes here.</p>
             </a>
           </Link>
 
           <Link href="/quiz1/pg3">
             <a className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600">
-              <h3 className="text-2xl font-bold">Quiz Template #3&rarr;</h3>
-              <p className="mt-4 text-xl">Try the third quiz!</p>
+              <h3 className="text-2xl font-bold">Buttons &rarr;</h3>
+              <p className="mt-4 text-xl">Things you press go here.</p>
             </a>
           </Link>  
 
           <Link href="/quiz1/pg4">
             <a className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600">
-              <h3 className="text-2xl font-bold">Quiz Template #4&rarr;</h3>
-              <p className="mt-4 text-xl">Try the fourth quiz!</p>
+              <h3 className="text-2xl font-bold">Text Input &rarr;</h3>
+              <p className="mt-4 text-xl">Single and multi-line text inputs</p>
             </a>
           </Link>  
 
@@ -49,7 +49,7 @@ export default function Home() {
       </main>
 
       <footer className="flex items-center justify-center w-full h-24 border-t">
-          Powered by ❤️
+          Powered by 🍬
       </footer>
     </div>
   )
