a79aa9993d96267c036be3bd55249b555e793154
tayv <48400779+tayv@users.noreply.github.com>
2022-04-30 20:30:30 -0600

    minor update to import

--- components/WrapperInput.tsx
+++ components/WrapperInput.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Input } from '@components/atoms/input'
 import { appendErrors } from 'react-hook-form'
-import UpdateInputState from '@components/UpdateInputState'
 
 const WrapperInput = ({
   name,
