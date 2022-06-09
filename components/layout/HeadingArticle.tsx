071690160c63511427ae041a17e3d47cef02e491
tayv <48400779+tayv@users.noreply.github.com>
2022-06-08 21:33:57 -0600

    Added secondary logic to other heading styles

--- components/layout/HeadingArticle.tsx
+++ components/layout/HeadingArticle.tsx
@@ -9,7 +9,7 @@ import { FC, ReactNode } from 'react'
 export type HeadingProps = {
     size: "h1" | "h2" | "h3" | "h4",
     text: string | number,
-    type?: "primary" | "secondary" // Add ability in future to reuse for multiple heading styles
+    type?: "primary" | "secondary" // Logic could use polishing. Will have problems if add a third style.
 }
 
 const HeadingArticle: FC<HeadingProps> = ({
@@ -24,13 +24,13 @@ const HeadingArticle: FC<HeadingProps> = ({
           return (type==="primary") ? <h1 className="text-2xl pt-2 pb-2">{text}</h1> : <h1 className="text-2xl pt-2 pb-2 text-slate-500">{text}</h1>
           
         case "h2":
-          return <h2 className="text-xl pt-2 pb-2">{text}</h2>
+          return (type==="primary") ? <h2 className="text-xl pt-2 pb-2">{text} </h2> : <h2 className="text-xl pt-2 pb-2 text-slate-500">{text} </h2> 
           
         case "h3":
-          return <h3 className="text-l pt-2 pb-2">{text}</h3>
+          return (type==="primary") ? <h3 className="text-l pt-2 pb-2">{text}</h3> : <h3 className="text-l pt-2 pb-2 text-slate-500">{text}</h3>
           
         case "h4":
-        return <h4 className="text-m pt-2 pb-2">{text}</h4>
+        return (type==="primary") ? <h4 className="text-m pt-2 pb-2">{text}</h4> : <h4 className="text-m pt-2 pb-2 text-slate-500">{text}</h4>
         
         default:
           return null;
