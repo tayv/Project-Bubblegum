79847096e5fc8a64e8571ebf62b63faec2efacb9
tayv <48400779+tayv@users.noreply.github.com>
2022-07-01 20:36:50 -0600

    Added logic to Breadcrumb component

--- components/layout/Breadcrumbs.tsx
+++ components/layout/Breadcrumbs.tsx
@@ -16,18 +16,22 @@ const Breadcrumbs: FC<BreadcrumbProps> = (props) => {
     return crumbs.map((crumb: BreadcrumbProps, index: number) => (
       <span key={index}>
         <span className="text-sm text-gray-500"> / </span>
-        
-        {crumb.active} {
-          <>
-            <span className="text-sm text-blue-500">{crumb.text}</span>
-          </>
-        }
-        {crumb.active} {
-          <Link href={crumb.path}>
-            <a className="inline underline no-underline text-sm text-gray-500 hover:decoration-dashed underline-offset-4 hover:text-blue-600 focus:text-blue-600">
-              {crumb.text}
-            </a>
-          </Link>
+        { 
+          (crumb.active)
+          // if the prop is active then render without a link as it's the current page
+          ? (
+            <>
+              <span className="text-sm text-blue-500">{crumb.text}</span>
+            </>
+          )
+          : (
+            // else render breadcrumb with a link
+            <Link href={crumb.path}>
+              <a className="inline underline no-underline text-sm text-gray-500 hover:decoration-dashed underline-offset-4 hover:text-blue-600 focus:text-blue-600">
+                {crumb.text}
+              </a>
+           </Link>
+          )
         }
       </span> 
         
