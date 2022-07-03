0b941432f77c9e69767b516641c51571b908b996
tayv <48400779+tayv@users.noreply.github.com>
2022-07-03 16:21:22 -0600

    fixed Breadcrumb active page text styling

--- components/layout/Breadcrumbs.tsx
+++ components/layout/Breadcrumbs.tsx
@@ -18,13 +18,13 @@ const Breadcrumbs = ({crumbs}: any) => {
           // if the prop is active then render without a link as it's the current page
           ? (
             <>
-              <span className="text-sm text-blue-500">{crumb.text}</span>
+              <span className="text-sm text-gray-500">{crumb.text}</span>
             </>
           )
           : (
             // else render breadcrumb with a link
             <Link href={crumb.path}>
-              <a className="inline underline no-underline text-sm text-gray-500 hover:decoration-dashed underline-offset-4 hover:text-blue-600 focus:text-blue-600">
+              <a className="inline underline no-underline text-sm text-blue-500 hover:decoration-dashed underline-offset-4 hover:text-blue-600 focus:text-blue-600">
                 {crumb.text}
               </a>
            </Link>
--- pages/forms/buttons.tsx
+++ pages/forms/buttons.tsx
@@ -21,14 +21,7 @@ export default function MyRadioGroup() {
   return (
     <> 
       <LayoutContainerSide>
-          <Breadcrumbs crumbs={crumbs} />
-
-          <Link href="#secondary">
-            <a className="p-6 mt-6 text-left border w-96 rounded-xl border-black hover:text-blue-600 focus:text-blue-600">
-              <h3 className="text-2xl font-bold">Text Input &rarr;</h3>
-              <p className="mt-4 text-xl">Single and multi-line text inputs</p>
-            </a>
-          </Link>  
+          <Breadcrumbs crumbs={crumbs} />  
 
           <HeadingArticle text="Buttons Heading" size="h1" type="primary"/> 
           <p>Explainer about buttons and when to use them goes here</p>
