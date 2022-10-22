78d5cdfa2071040ca2b3245d6887c97b1019ed61
tayv <48400779+tayv@users.noreply.github.com>
2022-10-21 19:12:02 -0600

    Minor update

--- components/layout/Label.tsx
+++ components/layout/Label.tsx
@@ -37,3 +37,6 @@ export default Label
 
 // Need to use function expression to render a switch statement in react. 
   // See https://stackoverflow.com/questions/55237619/expression-expected-in-react-using-switch-statement  
+
+// This Label component is used for input components that don't have a built in label
+  // Example: Checkboxes, Text Inputs, Text Areas.
