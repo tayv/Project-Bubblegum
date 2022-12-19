9b1ef898f81667265cc73d7f415c693ee346d005
tayv <48400779+tayv@users.noreply.github.com>
2022-12-18 20:25:54 -0700

    minor style refactors

--- components/atoms/DatePick.tsx
+++ components/atoms/DatePick.tsx
@@ -41,8 +41,8 @@ const DatePick = () => {
   }
 
   return (
-    <div className="pt-16">
-      <div className="max-w-md px-4 mx-auto sm:px-7 md:max-w-4xl md:px-6">
+    <div className="p-2 max-w-xl border-solid border-2">
+      <div className=" px-4 max-w-md mx-auto sm:px-7 md:max-w-4xl md:px-6">
         <div className="md:grid md:grid-cols-2 md:divide-x md:divide-gray-200">
           <div className="md:pr-14">
             <div className="flex items-center">
