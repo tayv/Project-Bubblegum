27071944ab1266ed2824f820bb490b139afeaf0b
tayv <48400779+tayv@users.noreply.github.com>
2022-12-22 19:09:41 -0700

    Updated tailwind to use data attributes for accordian states

--- components/atoms/DatePick.tsx
+++ components/atoms/DatePick.tsx
@@ -7,7 +7,7 @@ import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/20/solid'
 import Input, { InputProps } from '@atoms/Input'
 import SelectRadix from '@atoms/SelectRadix'
 import * as Select from '@radix-ui/react-select'
-import WrapperInput from '@components/controlled-wrappers/WrapperInput'
+import * as Accordion from '@radix-ui/react-accordion'
 import {
   add,
   eachDayOfInterval,
@@ -84,7 +84,133 @@ const DatePick: FC<InputProps> = ( { name , label, control } ) => {
   }
 
   return (
-    <>   
+    <> 
+
+    <Accordion.Root type="single" collapsible className=" " >
+      <Accordion.Item value="item-1" className=" ">
+        <Accordion.Header className="rounded-md shadow  data-[state=closed]:w-60 data-[state=open]:w-full">
+          <Accordion.Trigger className=""> Test Trigger </Accordion.Trigger>
+        </Accordion.Header>
+        <Accordion.Content className="data-[state=closed]:w-60 data-[state=open]:w-full">
+         <div className="py-2 border-solid border-2 w-full">
+      <div className="max-w-md mx-auto sm:px-7 md:max-w-4xl md:px-2">
+        <div className="md:grid md:grid-cols-2 md:divide-x md:divide-gray-200">
+          <div className="md:pr-14">
+            <div className="flex items-center">
+              <h2 className="flex-auto font-semibold text-gray-900">
+                {format(firstDaySelectedMonth, 'MMMM yyyy')}
+              </h2>
+              <button
+                type="button"
+                onClick={prevMonth}
+                className="-my-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
+              >
+                <span className="sr-only">Previous month</span>
+                <ChevronLeftIcon className="w-5 h-5" aria-hidden="true" />
+              </button>
+              <button
+                onClick={nextMonth}
+                type="button"
+                className="-my-1.5 -mr-1.5 ml-2 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
+              >
+                <span className="sr-only">Next month</span>
+                <ChevronRightIcon className="w-5 h-5" aria-hidden="true" />
+              </button>
+            </div>
+            <div className="grid grid-cols-7 mt-10 text-xs leading-6 text-center text-gray-500">
+              {/* // Can change the order of the days be re-ording the divs */}
+              <div>S</div>
+              <div>M</div>
+              <div>T</div>
+              <div>W</div>
+              <div>T</div>
+              <div>F</div>
+              <div>S</div>
+            </div>
+            <div className="grid grid-cols-7 mt-2 text-sm">
+              {/* // Map through the array of days in the selected month and render each day of the month as a button. 
+                  // Each button has its own conditional styling. */}
+              {daysInMonth.map((day, dayId) => (
+                <div
+                  key={day.toString()}
+                  className={classNames(
+                    dayId === 0 && firstDayStartingCol[getDay(day)], // First day equals index 0. So use date-fns getDay() as an index to start the first day of the month styling.
+                    'py-1.5'
+                  )}
+                >
+                  <button
+                    type="button"
+                    onClick={() => {
+                      setSelectedDay(day)
+                      setShowCalendar(false) // Remove if don't want calendar to close after selecting a day
+                    }} // when user clicks on a day, set that day as the selected day
+                    className={ classNames(
+                      // current day selected?
+                      isEqual(day, selectedDay) && 'text-white',
+                      // if day is selected + is today's date = make the background red
+                      isEqual(day, selectedDay) && isToday(day) && 'bg-indigo-500',
+                      // if day is selected + is NOT today's date = make the background gray
+                      isEqual(day, selectedDay) && !isToday(day) && 'bg-gray-900',
+                      // if the day is selected OR the day is today's date = make the font semi bold
+                      (isEqual(day, selectedDay) || isToday(day)) && 'font-semibold',
+
+                      // if current day is not selected = make the current day text red
+                      !isEqual(day, selectedDay) && isToday(day) && 'text-indigo-500',
+                       // if day is not selected + also not today's date + is in the current month = make the text gray
+                      !isEqual(day, selectedDay) && !isToday(day) && isSameMonth(day, firstDaySelectedMonth) && 'text-gray-900',
+                      // if day is not selected + also not today's date + is NOT in the current month = make the text lighter gray
+                      !isEqual(day, selectedDay) && !isToday(day) && !isSameMonth(day, firstDaySelectedMonth) && 'text-gray-400',
+                      // if day is NOT selected + user is hovering over it = make the background gray
+                      !isEqual(day, selectedDay) && 'hover:bg-gray-200',
+                     
+                      'mx-auto flex h-8 w-8 items-center justify-center rounded-full'
+                    ) }
+                  >
+                    {/* // display the day as a number */}
+                    <time dateTime={format(day, "yyyy-MM-dd")}>
+                      {format(day, "d")}
+                    </time>
+                  </button>
+
+                </div>
+              ))}
+            </div>
+          </div>
+          <section className="pl-2">
+            <h2 className="pb-1 text-gray-500 font-medium">Quick Jump</h2>
+            <ol>
+              <li className="text-emerald-500 pl-3 py-px"><button type="button" onClick={ () => setCurrentMonth(format(today, 'MMM-yyyy')) }>Today</button></li>
+              <li className="text-emerald-500 py-px"><button type="button" onClick={ () => jumpToDate(6) }>+ 6 months</button></li>
+              <li className="text-emerald-500 py-px"><button type="button" onClick={ () => jumpToDate(12) }>+ 1 year</button></li>
+              <li className="text-emerald-500 py-px"><button type="button" onClick={ () => jumpToDate(24) }>+ 2 years</button></li>
+            </ol>
+            <br/>
+            
+            {/* The Select.Root value syncs with state so it updates if user changes calendar to a new year. No name attribute since this field isn't submitted to server */}
+            <Select.Root  value={format(firstDaySelectedMonth,"yyyy")} defaultValue={format(firstDaySelectedMonth,"yyyy")} onValueChange={setNewYear} >
+              <Select.Trigger className="outline-none text-md border-solid border-2 border-slate-500 px-2 hover:bg-white">
+                <Select.Value placeholder="Year" />
+                <Select.Icon />
+              </Select.Trigger>
+        
+              <Select.Portal>
+                <Select.Content className="outline-none border-solid border-2 border-slate-500 bg-white py-1 px-2">
+                  <Select.ScrollUpButton />
+                  <Select.Viewport>
+                    { setYearRange(1999, 2025) }
+                  </Select.Viewport>
+                  <Select.ScrollDownButton />
+                </Select.Content>
+              </Select.Portal>
+            </Select.Root>
+          </section>
+        </div>
+      </div>
+         </div>
+        </Accordion.Content>
+      </Accordion.Item>
+    </Accordion.Root>    
+  
     <Input 
       name={name} 
       label={label} 
