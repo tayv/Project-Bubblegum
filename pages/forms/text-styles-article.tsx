d5b83852d771f288d6694fe8d7d216bfa44c52f9
tayv <48400779+tayv@users.noreply.github.com>
2023-01-05 22:19:20 -0700

    Added Paragraph Component details to article page

--- pages/forms/text-styles-article.tsx
+++ pages/forms/text-styles-article.tsx
@@ -32,11 +32,44 @@ const TextStylesArticle: FC = () => {
       <hr />
 
       <Section id="header" style="standard">
-        <Heading text="Paragraph" size="h3" type="primary"/>
+        <Heading text="Paragraph Component" size="h3" type="primary"/>
           <Paragraph 
-            text="🚧 Work in progress" 
+            text="The Paragraph component is used to standardize body text in the app. Currently it has two props: size and type. 
+                  The size prop has three variations: &quot;small&quot;, &quot;standard&quot;, and &quot;large&quot; as well as two type props: &quot;primary&quot; and &quot;secondary&quot;.
+                  It currently requires special characters to be escaped and cannot be passed links." 
             size="standard" 
             type="primary" 
+          />
+
+          <br/>
+          <Heading text="Example: Size Prop Variations" size="h4" type="primary"/>
+          <Paragraph 
+            text="This is a small size paragraph."
+            size="small" 
+            type="primary" 
+          />
+          <Paragraph 
+            text="This is a standard size paragraph."
+            size="standard" 
+            type="primary" 
+          />
+          <Paragraph 
+            text="This is a large size paragraph."
+            size="large" 
+            type="primary" 
+          />
+
+          <br/>
+          <Heading text="Example: Style Prop Variations" size="h4" type="primary"/>
+          <Paragraph 
+            text="This paragraph uses the primary style."
+            size="standard" 
+            type="primary" 
+          />
+          <Paragraph 
+            text="This paragraph uses the secondary style."
+            size="standard" 
+            type="secondary" 
           />
       </Section>
 
