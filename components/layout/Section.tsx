b73b73a7e49b37f41ad26e79fbf9f810245e4ee9
tayv <48400779+tayv@users.noreply.github.com>
2023-01-09 22:12:27 -0700

    minor padding refactor on Section

--- components/layout/Section.tsx
+++ components/layout/Section.tsx
@@ -11,9 +11,9 @@ export type SectionProps = {
 export type SectionStyle = "standard" | "blank" | "special"
 
 const sectionStyleMap: {[key in SectionStyle]: string} = {
-  standard: "bg-white drop-shadow", // css styles go here
+  standard: "p-4 bg-white drop-shadow", // css styles go here
   blank: "bg-transparent",
-  special: "bg-blue-100 drop-shadow",
+  special: "p-4 bg-blue-100 drop-shadow",
 }   
 
 const Section = (
@@ -28,7 +28,7 @@ const Section = (
     <section id={id} 
       className={
         classNames([
-          "mt-4 p-4 rounded-xl", // standard css styles go here. Peer is always required for label styling to work
+          "mt-4 rounded-xl", // standard css styles go here. Peer is always required for label styling to work
           sectionStyleMap[style], // to dynamically set styling for different radio types
           className, // prop
         ])
