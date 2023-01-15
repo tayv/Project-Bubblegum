d405a9c7f878accd6e185947497fc79bfdcc3bc7
tayv <48400779+tayv@users.noreply.github.com>
2023-01-14 17:12:40 -0700

    Color improvements

--- components/layout/SideNav.tsx
+++ components/layout/SideNav.tsx
@@ -16,13 +16,13 @@ const SideNav: FC<SideNavProps> = ({
   // The styles for the side nav are set here
   const sideNavStyleMap: {[key in SideNavStyle]: string} = {
     notSelected: "text-base text-gray-900 hover:text-gray-600", 
-    selected: "cursor-default text-base text-pink-600 border-l-2 border-pink-600"
+    selected: "cursor-default text-base text-pink-500 border-l-2 border-pink-500"
   } 
 
   return (
-    <nav className="flex flex-col w-64 min-w-fit m-4 px-4 pt-3 rounded-3xl backdrop-blur-lg bg-white/30 border border-white/20 drop-shadow-md" aria-label="Sidebar">
+    <nav className="flex flex-col w-64 min-w-fit m-4 px-4 pt-3 rounded-3xl backdrop-blur-lg bg-white/20 border border-white/20 drop-shadow-md" aria-label="Sidebar">
       <div>
-        <a href="/" className={classNames("flex items-center p-1 text-base font-semibold text-indigo-900 hover:text-indigo-600 ")}>
+        <a href="/" className={classNames("flex items-center p-1 text-base font-semibold text-gray-900 hover:text-gray-600 ")}>
           <span className="flex-1 ml-3 text-xl whitespace-nowrap">🏠  Home</span> 
         </a>
       </div>
