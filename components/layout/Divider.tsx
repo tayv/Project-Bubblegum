be6058a0f257ea549c265c892d752d1e35723e9f
tayv <48400779+tayv@users.noreply.github.com>
2023-01-16 20:08:53 -0700

    Added Divider variations

--- components/layout/Divider.tsx
+++ components/layout/Divider.tsx
@@ -2,24 +2,41 @@ import { FC } from 'react'
 import classNames from 'classnames'
 
 export type DividerProps = {
-  padding: "small" | "standard" | "large" | "none"
+  padding?: DividerPadding
   type?: "horizontal" | "vertical"
-  color?: "standard" | "white"
-  width?: "small" | "standard" | "large"
+  color?: DividerColor
+  size?: DividerSize
   className?: string
 }
 
-const dividerStyleMap: {[key in DividerProps["padding"]]: string} = {
+type DividerPadding = "small" | "standard" | "large" | "none"
+const dividerPaddingMap: {[key in DividerPadding]: string} = {
   small: "py-px", // prop specific css styles go here
   standard: "py-2",
   large: "py-4",
   none: "py-0" 
 }  
 
+type DividerColor = "standard" | "darkmode" | "white" | "black"
+const dividerColorMap: {[key in DividerColor]: string} = {
+  standard: "bg-neutral-700",
+  darkmode: "bg-neutral-300",
+  white: "bg-white",
+  black: "bg-black"
+}
+
+type DividerSize = "small" | "standard" | "large" | "xlarge"
+const dividerSizeMap: {[key in DividerSize]: string} = {
+  small: "h-px",
+  standard: "h-0.5",
+  large: "h-1",
+  xlarge: "h-2"
+}
+
 const Divider: FC<DividerProps> = ({
   type = "horizontal",
   color = "standard",
-  width = "standard",
+  size = "standard",
   padding = "standard",
   ...props
 }) => {
@@ -28,11 +45,18 @@ const Divider: FC<DividerProps> = ({
      <div className={
       classNames([
         "flex w-full", // standard css styles 
-        dividerStyleMap[padding], // dynamically set styling based on padding prop
-        props.className, // custom styling passed as prop
+        dividerPaddingMap[padding], // dynamically set styling based on padding prop
       ])
     }>
-      <div className="bg-black h-0.5 w-full rounded-full"></div>
+      <div className={
+        classNames([
+          "bg-black h-0.5 w-full rounded-full", 
+          dividerColorMap[color], 
+          dividerSizeMap[size], 
+          props.className, // custom styling passed as prop
+        ])
+       }>
+      </div>
      </div>
   )
 }
