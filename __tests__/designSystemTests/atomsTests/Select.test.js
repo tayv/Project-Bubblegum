699fce418f152e0368ee3b02776e3ec7f705fbef
tayv <48400779+tayv@users.noreply.github.com>
2023-03-26 19:03:19 -0600

    added folder structure to __tests__

---
+++ __tests__/designSystemTests/atomsTests/Select.test.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import SelectRadix from '../../../designSystem/atoms/SelectRadix' // why doesn't @atoms/SelectRadix work here?
+
+describe('SelectRadix component', () => {
+ 
+  test('renders only once', () => {
+    const selectRef = React.createRef()
+
+    const { getByTestId } = render(
+    
+      <SelectRadix 
+       // onValueChange={onChange}
+        value={"test-value"}
+        forwardedRef={selectRef}
+        placeholder="test-placeholder"
+        itemOptions={ [
+            {value:"first", labelText:"firstText", separator: false}, 
+            {value:"second", labelText:"secondText", separator: true}
+          ] }
+       // data-testid="test-select"
+      />
+      
+    )
+    
+    // Verify that the SelectRadix component renders properly
+
+      // Attempt 1:
+    expect(selectRef.current).toBe(true) // this fails, returns null
+
+     // Attempt 2:
+   // expect(getByTestId('test-select')).toBeInTheDocument() // this fails, unable to find an element by: [data-testid="test-select"]
+
+  })
+})
--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,19 @@ const nextJest = require('next/jest')
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  // These need to match tsconfig.json paths
   dir: './',
+  '^@designSystem/(.*)$': '<rootDir>/designSystem/$1',
+  '^@atoms/(.*)$': '<rootDir>/designSystem/atoms/$1',
+  '^@molecules/(.*)$': '<rootDir>/designSystem/molecules/$1',
+  '^@organisms/(.*)$': '<rootDir>/designSystem/organisms/$1',
+  '^@forms/(.*)$': '<rootDir>/designSystem/forms/$1',
+  '^@layouts/(.*)$': '<rootDir>/designSystem/layouts/$1',
+  '^@styles/(.*)$': '<rootDir>/styles/$1',
+  '^@helpers/(.*)$': '<rootDir>/designSystem/molecules/$1',
+  '^@templates/(.*)$': '<rootDir>/designSystem/templates/$1',
+  '^@docs/(.*)$': '<rootDir>/pages/docs/$1',
+  '^@restricted/(.*)$': '<rootDir>/pages/restricted/$1',
 })
 
 // Add any custom config to be passed to Jest
