2b11668d3cc97c5ec4ae2a18f07870d54eca8694
tayv <48400779+tayv@users.noreply.github.com>
2023-05-29 22:10:04 -0600

    setup app router basics

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+//import LayoutContainerSide from "@designSystem/layouts/LayoutContainerSide"
+import Breadcrumbs from "@designSystem/molecules/Breadcrumbs"
+import Heading from "@designSystem/atoms/Heading"
+import Paragraph from "@designSystem/atoms/Paragraph"
+import CardSection from "@designSystem/molecules/CardSection"
+import Space from "@designSystem/atoms/Space"
+
+export default function Home() {
+  return (
+    <>
+     <p>This is a test home page with app router.</p>
+    </>
+  )
+}
