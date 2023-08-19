be35bfb02d3e01b335ae4a3bec7856e8edb1f2cd
tayv <48400779+tayv@users.noreply.github.com>
2023-08-18 22:32:50 -0600

    centered stepper buttons and added toolbar button

--- app/components/form/formControl/FormTest2.tsx
+++ app/components/form/formControl/FormTest2.tsx
@@ -15,6 +15,7 @@ import {
   ArrowBigDownDash,
   ArrowBigUp,
   ArrowBigUpDash,
+  Wrench,
 } from "lucide-react"
 import dynamic from "next/dynamic"
 import { pdfStyles } from "@product2/_pdfHelpers/pdfStyles"
@@ -97,14 +98,26 @@ const FormTest2: FC<FormProps> = ({
             </PDFViewer>
           </ModalViewDoc>
         </div>
-        <div className="z-10 fixed bottom-0 w-full flex justify-center">
+
+        <div className="z-10 fixed bottom-0 left-0 w-full flex justify-center items-center">
+          <div className="flex flex-1 pl-6">
+            {/* Invisible div is needed so flexbox can center the stepper buttons while right aligning the toolbar button*/}
+            {/* IMPORTANT: left padding here must match right padding on toolbar button so steppers are centered.*/}
+          </div>
           <div className="lg:hidden flex flex-row gap-4 items-center m-4 px-6 py-3 border border-slate-300 rounded-full bg-white drop-shadow-md max-w-md">
-            <button className="max-w-xs bottom-0 py-2 border-2 border-slate-500 rounded-full p-2 ">
+            <button className="max-w-xs bottom-0 p-2 border-2 border-slate-500 rounded-full ">
               <ArrowBigUpDash className="text-slate-500" />
             </button>
 
-            <button className="max-w-xs bottom-0 py-2 shadow border-2 border-sky-600 bg-sky-500 rounded-full p-2 ">
+            <button className="max-w-xs bottom-0 p-2 shadow border-2 border-sky-600 bg-sky-500 rounded-full ">
               <ArrowBigDownDash fill="white" stroke="white" />
+            </button>
+          </div>
+
+          {/* Toolbar options don't need to be collapsed on desktop so hide this for large screens*/}
+          <div className="flex flex-1 justify-end pr-6 lg:hidden">
+            <button className="max-w-xs bottom-0 p-2  border bg-slate-100 border-slate-300 rounded-full drop-shadow ">
+              <Wrench className="text-slate-500" />
             </button>
           </div>
         </div>
