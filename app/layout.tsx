8dd8fa9187a5d15d2ecdd1e4b8c4c143e5d6092a
tayv <48400779+tayv@users.noreply.github.com>
2023-09-08 16:57:02 -0600

    Clerk Auth working on whole app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "@styles/globals.css" // These styles apply to every route in the application
 import { Metadata } from "next"
 import NavSideBar from "./components/ui/NavSideBar"
+import { ClerkProvider } from "@clerk/nextjs"
 
 export const metadata: Metadata = {
   title: "Home",
@@ -15,31 +16,33 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className="bg-neutral-100 overscroll-none max-w-screen">
-        {/* Need separate div so don't conflict with child flex items */}
-        <div className="flex justify-center items-center h-screen">
-          {/* use lg:flex-row because want the nav to move to the side on laptops and above */}
-          <div className="flex flex-col lg:flex-row h-screen">
-            {/* <div className="flex flex-1 overflow-hidden"> */}
-            <NavSideBar
-              // NOTE: These titles must be unique to avoid react list key error
-              articleList={[
-                { title: "Products", groupTitle: true },
-                { title: "Product Demo (PDF Builder)", path: "/product2" },
-                { title: "Placeholder A", path: "/" },
-                { title: "Placeholder B", path: "/" },
-                { title: "Placeholder C", path: "/" },
-                { title: "Placeholder D", path: "/" },
-              ]}
-            />
-            {/* Only mobile styles need more top padding to offset fixed header */}
-            <main className="w-full max-w-screen pt-16 pb-32 lg:px-6 lg:py-3 lg:pt-2">
-              {children}
-            </main>
+    <ClerkProvider>
+      <html lang="en">
+        <body className="bg-neutral-100 overscroll-none max-w-screen">
+          {/* Need separate div so don't conflict with child flex items */}
+          <div className="flex justify-center items-center h-screen">
+            {/* use lg:flex-row because want the nav to move to the side on laptops and above */}
+            <div className="flex flex-col lg:flex-row h-screen">
+              {/* <div className="flex flex-1 overflow-hidden"> */}
+              <NavSideBar
+                // NOTE: These titles must be unique to avoid react list key error
+                articleList={[
+                  { title: "Products", groupTitle: true },
+                  { title: "Product Demo (PDF Builder)", path: "/product2" },
+                  { title: "Placeholder A", path: "/" },
+                  { title: "Placeholder B", path: "/" },
+                  { title: "Placeholder C", path: "/" },
+                  { title: "Placeholder D", path: "/" },
+                ]}
+              />
+              {/* Only mobile styles need more top padding to offset fixed header */}
+              <main className="w-full max-w-screen pt-16 pb-32 lg:px-6 lg:py-3 lg:pt-2">
+                {children}
+              </main>
+            </div>
           </div>
-        </div>
-      </body>
-    </html>
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
