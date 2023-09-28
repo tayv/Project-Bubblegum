9c3dd9b7da21185b2f4904fac12701d3f7465373
tayv <48400779+tayv@users.noreply.github.com>
2023-09-27 22:35:29 -0600

    New draft schema working

--- app/api/inquiry/route.ts
+++ app/api/inquiry/route.ts
@@ -7,9 +7,20 @@ export async function POST(request: Request) {
   try {
     const body = await request.json()
 
-    const newEntry = await prisma.inquiry.create({
+    const newEntry = await prisma.docData.create({
       data: {
-        name: "This is a hardcoded test string. Need to update schema to capture form input data",
+        status: "DRAFT",
+        product: {
+          connect: {
+            id: 1,
+          },
+        },
+        user: {
+          connect: {
+            userId: 1,
+          },
+        },
+        docName: body.textExample,
         formData: body,
       },
     })
@@ -23,32 +34,3 @@ export async function POST(request: Request) {
     })
   }
 }
-
-// Page router API endpoint:
-// export default async function handler(req: any, res: any) {
-//   if (req.method === "POST") {
-//     return await createInquiry(req, res)
-//   } else {
-//     return res
-//       .status(405)
-//       .json({ message: "Method not allowed", success: false })
-//   }
-// }
-
-// async function createInquiry(req: any, res: any) {
-//   const body = req.body
-//   try {
-//     const newEntry = await prisma.inquiry.create({
-//       data: {
-//         name: "This is a hardcoded test string. Need to update schema to capture form input data",
-//         formData: body,
-//       },
-//     })
-//     return res.status(200).json(newEntry, { success: true })
-//   } catch (error) {
-//     console.error("Request error", error)
-//     res
-//       .status(500)
-//       .json({ error: "AAAHHHHHH...error creating question", success: false })
-//   }
-// }
