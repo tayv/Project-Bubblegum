ddf994c658a125de2f755add299ab7f2a2c6e691
tayv <48400779+tayv@users.noreply.github.com>
2024-03-04 21:10:13 -0700

    Jest tests now run

---
+++ packages/ui/jest.config.js
@@ -0,0 +1,11 @@
+console.log("Resolved path:", require.resolve("config-tests/jest-next"))
+const base = require("config-tests/jest-next")
+
+module.exports = {
+  ...base,
+  name: "shared-ui",
+  displayName: "Shared UI Tests",
+  moduleNameMapper: {
+    "^config-tests/(.*)$": "<rootDir>/packages/config-tests/$1",
+  },
+}
